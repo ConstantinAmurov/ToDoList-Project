f4d57463574a42227493aa3d72b3206797a259f5
CostelMD <amurov.constantin@gmail.com>
2020-09-03 16:08:19 +0300

    Initialized the mongoose package, started the server and created the defaultTasks

--- app.js
+++ app.js
@@ -2,24 +2,61 @@ const express = require("express");
 const port = 3000;
 const https = require("https");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
-console.log(date);
+
+
 const app = express();
-let tasks = ["Learn EJS", "Create a project", "Find an intership"];
-let workTasks = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+const url = "mongodb://localhost:27017/todoListDB";
+//const client = new MongoClient(uri,{ useUnifiedTopology: true } );
+try {
+  mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }, () => {
+
+    const itemsSchema = {
+      name: String
+    };
+    const Item = mongoose.model("Item", itemsSchema);
+
+
+    const task_1 = new Item({
+      name: "Welcome to your To Do List"
+    });
+    const task_2 = new Item({
+      name: "Hit the + button to add a new task"
+    });
+    const task_3 = new Item({
+      name: " <---- Hit this to delete a task"
+    });
+    const defaultTasks = [task_1, task_2, task_3];
+    Item.insertMany(defaultTasks, (err) => {
+      if (err) console.log(err);
+      else console.log("Succesfully added defaultTasks to DB");
+    }
+    );
+
+  });
+
+
+} catch (error) {
+  console.log("Could not connect");
+}
+
 app.get("/", function(req, res) {
 
   let day = date.getDate();
 
   res.render("list", {
-    listTitle: day,
+    listTitle: "Today",
     newListTask: tasks
   });
 });
