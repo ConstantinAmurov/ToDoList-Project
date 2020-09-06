70446e0baa90abff2c3198c2a734ec5f52fa3ad3
CostelMD <amurov.constantin@gmail.com>
2020-09-06 15:40:08 +0300

    1) Created Custom Lists using Express Route Parameters 2) Adding and deleting tasks from custom lists

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port = 3000;
 const https = require("https");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 
 
@@ -43,7 +44,11 @@ try {
     });
     const defaultTasks = [task_1, task_2, task_3];
 
-
+    const listSchema = {
+      name: String,
+      items: [itemsSchema]
+    }
+    const List = mongoose.model("List", listSchema);
 
 
     app.get("/", function(req, res) {
@@ -66,32 +71,83 @@ try {
       });
     });
 
-    app.get("/work", (req, res) => {
-      res.render("list", {
-        listTitle: "Work",
-        newListTask: workTasks
+    app.get("/:customListName", (req, res) => {
+      const customListName = _.capitalize(req.params.customListName);
+      List.findOne({
+        name: customListName
+      }, (err, foundList) => {
+        if (!err) {
+          if (!foundList) { //if foundList is empty create a new List
+            const list = new List({
+              name: customListName,
+              items: defaultTasks
+            });
+            list.save();
+            res.redirect("/" + customListName);
+          } else {
+
+            res.render("list", {
+              listTitle: foundList.name,
+              newListTask: foundList.items
+            })
+          }
+        }
+
       });
     });
 
     app.post("/", function(req, res) {
-      let taskName = req.body.newTask;
+      const taskName = req.body.newTask;
+      const listName = _.capitalize(req.body.list);
       const task = new Item({
         name: taskName
       });
-
-      task.save(function(err) {
-        if (err) console.log(err);
-        else console.log("Succesfully saved task to db");
-      });
-      res.redirect("/");
+      if (listName === "Today") {
+        task.save(function(err) {
+          if (err) console.log(err);
+          else console.log("Succesfully saved task to db");
+        });
+        res.redirect("/");
+      } else {
+        List.findOne({
+          name: listName
+        }, (err, foundList) => {
+          foundList.items.push(task);
+          foundList.save();
+          res.redirect("/" + listName);
+        })
+      }
     });
-    app.post("/delete",(req,res)=> {
-      const checkedItemId= req.body.checkbox;
-      Item.findByIdAndRemove({_id: checkedItemId},(err)=> {
-        if(err) console.log(err);
-        else console.log("Succesfully deleted the item with ID :" + checkedItemId);
-      });
-      res.redirect("/");
+
+    app.post("/delete", (req, res) => {
+      const checkedItemId = req.body.checkbox;
+      const listName = _.capitalize(req.body.listName);
+      if (listName === "Today") {
+        Item.findByIdAndRemove({
+          _id: checkedItemId
+        }, (err) => {
+          if (err) console.log(err);
+          else console.log("Succesfully deleted the item with ID :" + checkedItemId);
+        });
+        res.redirect("/");
+      } else {
+        List.findOneAndUpdate({
+          name: listName
+        }, {
+          $pull: {
+            items: { // after pull we specify the particular field(array ) in our case that is the itesm array
+              _id: checkedItemId // we specify the data and what it is equal to
+            }
+          }
+        }, (err, foundList) => { // this is the callback, obligatory
+          if (!err) {
+            res.redirect("/" + listName);
+          }
+        })
+
+      }
+
+
     });
 
     app.get("/about", function(req, res) {
